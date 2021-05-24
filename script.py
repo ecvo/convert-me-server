0b926425da36b2ca04887033332817a077e18467
Mark Kravets <49925339+ecvo@users.noreply.github.com>
2021-05-24 21:38:01 +0300

    Refactor Python Code
    Refactor Python Script

--- script.py
+++ script.py
@@ -2,14 +2,7 @@ import os
 import pandoc
 import pypandoc
 
-conout = pypandoc.convert_file('./uploads/1.docx', 'odt', outputfile="./output/1.odt")
-conout = pypandoc.convert_file('./uploads/2.docx', 'odt', outputfile="./output/2.odt")
-conout = pypandoc.convert_file('./uploads/3.docx', 'odt', outputfile="./output/3.odt")
-conout = pypandoc.convert_file('./uploads/4.docx', 'odt', outputfile="./output/4.odt")
-conout = pypandoc.convert_file('./uploads/5.docx', 'odt', outputfile="./output/5.odt")
-conout = pypandoc.convert_file('./uploads/6.docx', 'odt', outputfile="./output/6.odt")
-conout = pypandoc.convert_file('./uploads/7.docx', 'odt', outputfile="./output/7.odt")
-conout = pypandoc.convert_file('./uploads/8.docx', 'odt', outputfile="./output/8.odt")
-
+for x in range (1, 100):
+    conout = pypandoc.convert_file(f'./uploads/{str(x)}.docx', 'odt', outputfile=f'./output/{str(x)}.odt')
 
 assert conout == ''
