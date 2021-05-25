a6c062979ebbf4c1797fa50e4366c3d3a2e5418f
Mark Kravets <49925339+ecvo@users.noreply.github.com>
2021-05-25 10:09:17 +0300

    Update index.js
    Added ability to create folders with unique id's same as executing python script for convert, sending files to end user

--- index.js
+++ index.js
@@ -1,57 +1,65 @@
 const { SSL_OP_EPHEMERAL_RSA } = require('constants');
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const app = express();
-const fs = require('fs');
 const path = require('path');
 const zipper = require('zip-local');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
 
 
 const PORT = 8000;
 app.use('/', express.static(__dirname + 'dist'));
 
+app.use(express.json());       
+app.use(bodyParser.json());
 app.use(fileUpload());
 
 app.post('/upload', async function (req, res) {
-  console.dir(req.ip)
   let sampleFile;
   let uploadPath;
+  
+  console.log(JSON.stringify(req.body));
 
+  const userId = req.body.userId;
 
-  console.log(`req.files >>>`,req.files) // eslint-disable-line
+      console.log('used_id = ',userId)
+      
+      console.log(`req.files >>>`,req.files) // eslint-disable-line
 
-  const directory = __dirname + '/uploads';
+  /*const directory = __dirname + `/data/`;
 
-  fs.readdir(directory, (err, files) => {
-    if (err) throw err;
-
-    for (const file of files) {
-      fs.unlink(path.join(directory, file), err => {
-        if (err) throw err;
-      });
+  fs.rmdir(directory, { recursive: true }, (err) => {
+    if (err) {
+        throw err;
     }
-  });
+    console.log(`${directory} is deleted!`);
+});*/ //delete
 
-  const directory1 = __dirname + '/output';
-
-  fs.readdir(directory1, (err, files) => {
-    if (err) throw err;
-
-    for (const file of files) {
-      fs.unlink(path.join(directory1, file), err => {
-        if (err) throw err;
-      });
+    const dir = './data/' + userId
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
-  });
+
+    const dir1 = './data/' + userId + '/uploads'
+    if (!fs.existsSync(dir1)) {
+      fs.mkdirSync(dir1);
+    }
+
+    const dir2 = './data/' + userId + '/output'
+    if (!fs.existsSync(dir2)) {
+      fs.mkdirSync(dir2);
+    }
+
 
     setTimeout(() => {
       sampleFile = req.files.sampleFile;
       files = [].concat(sampleFile);
-      
-      uploadPath = __dirname + '/uploads/';
+
+      uploadPath = __dirname + `/data/${userId}` + '/uploads/';
   
       for (let i = 0; i < files.length; i++) {
-        files[i].name = (i+1) + '.docx'
+        files[i].name = i + '.docx'
   
         files[i].mv(uploadPath + files[i].name, function (err) {
           if (err) {
@@ -62,15 +70,20 @@ app.post('/upload', async function (req, res) {
       const { spawn } = require('child_process');
       const pyProg = spawn('python', ['script.py']);
       console.log(`converted`);
+      setTimeout(() => {
+        //zip all /output files to .zip
+        zipper.sync.zip(__dirname + '/data/'+`/${userId}/output/`).compress().save(__dirname + '/data/'+`/${userId}/output/1.zip`);
+      }, 2000);
     }, 1000);
+    res.send({userId: userId})
 });
+  
 
 app.get('/download', async function (req, res) {
-
-  //zip all /output files to .zip
-  zipper.sync.zip("./output").compress().save("./output/1.zip");
+  var userId = req.body.userId
+  console.log('used_id = ',userId)
   //throw output to client-side
-  const file = `${__dirname}/output/1.zip`;
+  const file = `${__dirname}/data/${userId}/output/1.zip`;
   res.sendFile(file);
 });
 
