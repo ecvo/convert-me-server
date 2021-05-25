6351ff0da2306e3e597a548d2cc9ee2b749ed78f
Mark Kravets <49925339+ecvo@users.noreply.github.com>
2021-05-25 10:06:22 +0300

    Update Python script
    New for cycle with finding unique id names and count files to convert

--- script.py
+++ script.py
@@ -2,7 +2,12 @@ import os
 import pandoc
 import pypandoc
 
-for x in range (1, 100):
-    conout = pypandoc.convert_file(f'./uploads/{str(x)}.docx', 'odt', outputfile=f'./output/{str(x)}.odt')
+path = './data'
 
-assert conout == ''
+for filename in os.listdir(path):
+    file = f'./data/{filename}/uploads'
+    print(len(os.listdir(file)))
+    ster = len(os.listdir(file))
+    for x in range (0, ster):
+        conout = pypandoc.convert_file(f'./data/{filename}/uploads/{str(x)}.docx', 'odt', outputfile=f'./data/{filename}/output/{str(x)}.odt')
+        assert conout == ''
